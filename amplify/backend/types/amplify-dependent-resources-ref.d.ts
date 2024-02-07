b6b175ff48c87d7db868569ad656fc47654b36bf
Daniel Radut <danielradut@Daniels-MBP.attlocal.net>
2024-02-07 12:14:18 -0500

    Added graphql and storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,18 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "api": {
+        "amplifydemo": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "amplifyDemoImages": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
